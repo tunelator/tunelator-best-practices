f1d6fbb108152cbc655b8745caf0cd4866fe6620
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-05-26 21:40:03 -0300

    feat: begin configuration to deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,18 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Tunelator Best Practices',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://tunelator.github.io/',
+  baseUrl: '/tunelator-best-practices/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Tunelator', // Usually your GitHub org/user name.
+  projectName: 'tunelator-best-practices', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/tunelator/tunelator-best-practices/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
